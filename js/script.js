c587f9d90efb431cc95dd43588dbf3ec8cf4e89f
JUNG Jules <jung.jules@pm.me>
2023-01-01 11:56:07 +0100

    FIx #6

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 class Countdown {
-    constructor(date) {
+    constructor(date, message) {
+        this.message = message;
         this.date = date;
     }
 
+    set interval(intervalId) {
+        this.intervalId = intervalId;
+    }
+
     get remaining() {
-        return this.date.getTime() - Date.now()
+        if (this.date.getTime() - Date.now() >= 0) {
+            return parseInt((this.date.getTime() - Date.now()) / 1000)
+        } else {
+            clearInterval(this.intervalId)
+            return this.message
+        }
     }
 }
 
 params = new URLSearchParams(window.location.search)
-countdown = new Countdown(new Date(`${params.get("date")} ${params.get("time")}`))
-setInterval(() => {
-    document.getElementById("remaining").innerHTML = parseInt(countdown.remaining / 1000)
+countdown = new Countdown(new Date(`${params.get("date")} ${params.get("time")}`), params.get("message"))
+countdown.interval = setInterval(() => {
+    document.getElementById("remaining").innerHTML = countdown.remaining
 }, 100)
